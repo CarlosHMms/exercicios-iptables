33b458255868a21c2f47e72cc542c23dc7cf18b8
Carlos Henrique Mota Martins <107642033+CarlosHMms@users.noreply.github.com>
2024-04-01 19:34:53 -0300

    Finalizando

--- iptables.sh
+++ iptables.sh
@@ -14,7 +14,6 @@ iptables -A OUTPUT -o lo -j ACCEPT
 # Estabeleça a política DROP (restritiva) para as chains INPUT e FORWARD da tabela filter.
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
-iptables -P OUTPUT ACCEPT
 
 # Possibilite que usuários da rede interna possam acessar o serviço WWW, tanto na porta (TCP) 80 como na 443. Não esqueça de realizar NAT já que os usuários internos não possuem um endereço IP válido
 
@@ -33,3 +32,26 @@ iptables -A FORWARD -i enp0s8 -p tcp --dport 80 -d www.jogosonline.com.br -j DRO
 # permita que o firewall receba pacotes do tipo ICMP echo-request (ping), porém, limite a 5 pacotes por segundo
 
 iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 5/s -j ACCEPT
+
+# Permita que tanto a rede interna como a DMZ possam realizar consultas ao DNS externo, bem como, receber os resultados das mesmas
+
+iptables -A INPUT -i 10.1.1.0/24  TCP --dport 53 -j ACCEPT
+iptables -A OUTPUT -i 10.1.1.0/24  TCP --dport 53 -j ACCEPT
+
+iptables -A INPUT -i 10.1.1.0/24  UDP --dport 53 -j ACCEPT
+iptables -A OUTPUT -i 10.1.1.0/24  UDP --dport 53 -j ACCEPT
+
+# Permita o tráfego TCP destinado à máquina 192.168.1.100 (DMZ) na porta 80, vindo de qualquer rede (Interna ou Externa).
+
+sudo iptables -A INPUT -p tcp --dport 80 -d 192.168.1.100 -j ACCEPT
+sudo iptables -A FORWARD -p tcp --dport 80 -d 192.168.1.100 -j ACCEPT
+
+# Redirecione pacotes TCP destinados ao IP 200.20.5.1 porta 80, para a máquina 192.168.1.100 que está localizado na DMZ.
+
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -d 200.20.5.1 -j DNAT --to-destination 192.168.1.100:80
+sudo iptables -t nat -A POSTROUTING -d 192.168.1.100 -p tcp --dport 80 -j MASQUERADE
+
+# Faça com que a máquina 192.168.1.100 consiga responder os pacotes TCP recebidos na porta 80 corretamente
+
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
